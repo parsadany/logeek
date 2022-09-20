47d7cf69b89b5d48104b32989b7e9c9dab5e10a9
parsadany <parsadany@gmail.com>
2022-09-20 17:24:17 +0430

    add home index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'shared/variables.php';
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url='<?php print($protocol.$site_uri.$home_address) ?>'" />
+  </head>
+  <body>
+    <p>Please follow <a href="https://www.w3docs.com">this link</a>.</p>
+  </body>
+</html>
